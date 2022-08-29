2b717a3cfea370d2286b0b34cad42d7956cdcbc9
Audy Wallace Siegle <audywallace@Audys-MBP.lan>
2022-08-29 22:08:23 +0700

    finish implementing crud logic

--- src/task.js
+++ src/task.js
@@ -3,7 +3,8 @@ class TaskElement extends HTMLElement {
     #shadowRoot;
     #template;
     #style;
-    #description
+    #description;
+    #errorMessage;
     constructor() {
         super();
         this.#shadowRoot = this.attachShadow({ mode: "open" });
@@ -12,32 +13,66 @@ class TaskElement extends HTMLElement {
         this.#template.innerHTML = `
             <div>
                 <span id="description"></span>
+                <input type="text" id="editInput" class="hide-element">
                 <button id="editButton">Edit</button>
                 <button id="deleteButton">Delete</button>
+                <button id="confirmEdit" class="hide-element">Ok</button>
+                <p class="hide-element error-message" id="errorMessage">Input Invalid!</p>
             </div>
         `;
 
         this.#style = document.createElement("style");
         this.#style.innerHTML = `
+        @import url("../css/task.css")
         `;
 
         this.#shadowRoot.appendChild(this.#style);
         this.#shadowRoot.appendChild(this.#template.content.cloneNode(true));
 
         this.#description = this.#shadowRoot.getElementById("description");
+        this.#errorMessage = this.#shadowRoot.getElementById("errorMessage");
     }
 
-    connectedCallback(){
+    isError(){
+        return this.#shadowRoot.getElementById("editInput").value === "";
+    }
+
+    validateInput(){
+        this.#shadowRoot.getElementById("confirmEdit").addEventListener("click",() => {
+            if (this.isError()) {
+                this.#errorMessage.setAttribute("class", "error-message");
+            } else{
+                this.#errorMessage.setAttribute("class", "hide-element");
+                Task.editDescription(+this.getAttribute("id"), this.#shadowRoot.getElementById("editInput").value);
+            }
+        })
+    }
+
+    connectedCallback() {
         this.deleteFromDOM();
+        this.editMode();
+        this.validateInput();    
     }
 
-    attachDescription(task){
+    attachDescription(task) {
         this.#description.innerText = task;
     }
 
-    deleteFromDOM(){
+    deleteFromDOM() {
         this.#shadowRoot.getElementById("deleteButton").addEventListener("click", () => {
             Task.deleteFromStorage(+this.getAttribute("id"));
+        });
+    }
+
+    editMode() {
+        this.#shadowRoot.getElementById("editButton").addEventListener("click", () => {
+            const description = this.#description.innerText;
+            this.#shadowRoot.getElementById("editButton").setAttribute("class", "hide-element");
+            this.#shadowRoot.getElementById("deleteButton").setAttribute("class", "hide-element");
+            this.#description.setAttribute("class", "hide-element")
+            this.#shadowRoot.getElementById("confirmEdit").removeAttribute("class");
+            this.#shadowRoot.getElementById("editInput").removeAttribute("class");
+            this.#shadowRoot.getElementById("editInput").value = description;
         });
     }
 }
